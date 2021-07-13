8c00153c8c900158c8fab7aefa8981a0cfa707a9
Jayash Bhandary <jayash@Jayashs-iMac.local>
2021-07-13 16:09:04 +0530

    res

--- index.js
+++ index.js
@@ -12,16 +12,18 @@ app.get("/", (req,res)=>{
 });
 
 
-app.get('/db', async(req,res)=>{
-    try{
-        const client = await pool.connect()
-        const result = await client.query('SELECT * FROM test_table');
-        res.json(result);
-    }catch(err) {
-        console.error(err);
-        res.send("Error " + err)
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.json(results)
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
     }
-})
+  })
 
 
 app.get("/songs/:id", (req,res)=>{
